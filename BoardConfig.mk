034b4ba73ba4b9263ffd3cc66cb12cbd66c96a34
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    xaga: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,3 +42,6 @@ VENDOR_SECURITY_PATCH := 2024-09-01
 
 # Inherit the proprietary files
 include vendor/xiaomi/xaga/BoardConfigVendor.mk
+
+# Inherit from proprietary files for miuicamera
+-include vendor/xiaomi/miuicamera-xaga/BoardConfig.mk
